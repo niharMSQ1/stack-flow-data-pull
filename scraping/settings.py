b01eb9307b58dfdbe6f8cff181e397f5117c91e6
unknown <nihar.m@sq1.security>
2025-06-23 10:51:23 +0530

    bugs

--- scraping/settings.py
+++ scraping/settings.py
@@ -74,15 +74,24 @@ WSGI_APPLICATION = 'scraping.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'scrap',
+        'USER': 'your_user',
+        'PASSWORD': 'your_pass',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
-
-
 
 
 # Password validation
